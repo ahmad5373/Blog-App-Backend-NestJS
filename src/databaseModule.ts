7f887c107c18f374ca1becc52006ea7649d9a88a
ahmad5373 <ahmadraxa550@gmail.com>
2024-09-26 12:55:36 +0500

    update database

--- src/databaseModule.ts
+++ src/databaseModule.ts
@@ -10,13 +10,12 @@ dotenv.config();
     imports: [
         TypeOrmModule.forRoot({
             type: 'postgres',
-            host: process.env.DB_HOST,
-            port: 5432,
-            username: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
+            url: process.env.DATABASE_URL,  
             entities: [User, Post, Comment],
-            synchronize: true,
+            synchronize: true, 
+            ssl: {
+                rejectUnauthorized: false,
+            },
         }),
     ],
 })
